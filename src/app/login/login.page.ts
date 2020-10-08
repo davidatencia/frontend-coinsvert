a8e8d7db0719e139c53ce0ad3c2ba64f5723d1b6
Juan Felipe Calao Pérez <jfcalao@uninorte.edu.co>
2020-10-07 22:27:25 -0500

    toggle buttons

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,13 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
   test(){
     console.log('Testing')
+    const buttons = document.getElementsByClassName("button")
+    if(buttons[0].classList.contains("clicked")){
+      buttons[0].classList.remove("clicked")
+      buttons[1].classList.add("clicked")
+    } else {
+      buttons[0].classList.add("clicked")
+      buttons[1].classList.remove("clicked")
+    }
   }
 
 }
