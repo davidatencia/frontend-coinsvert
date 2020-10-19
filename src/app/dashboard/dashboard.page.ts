d968fa899b1ebdb75179a270048d4ee2e0f47694
Roy Manotas <rjmanotasd@misena.edu.co>
2020-10-19 12:18:18 -0500

    responsive grid 2

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
+  test(){
+    console.log('Testing')
+    const buttons = document.getElementsByClassName("switch-button")
+    if(buttons[0].classList.contains("clicked")){
+      buttons[0].classList.remove("clicked")
+      buttons[1].classList.add("clicked")
+    } else {
+      buttons[0].classList.add("clicked")
+      buttons[1].classList.remove("clicked")
+    }
+  }
 }
